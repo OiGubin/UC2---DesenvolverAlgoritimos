711ac6e5478d29f503f0d7cfa532b8f38243124a
GUSTAVO ALEXANDRE GONSALVES DOS SANTOS <03050192011@senacrs.edu.br>
2026-03-23 14:37:25 -0300

    subindo os arquivos para o GitHub

---
+++ Variavel/Variaveis01.js
@@ -0,0 +1,54 @@
+/*console.log("Hello World!!!")
+console.log("Hello World!!!!!!!")*/
+
+//==================================
+//1. DECLARAÇÃO DE VARIÁVEIS
+//==================================
+
+/* 
+Em JavaScript podemos declarar variáveis usando:
+let (que pode mudar)
+const (constante, não pode ser reatribuída) */
+
+let nome = "Gustavo"
+let idade = 15 
+const país = "Brasil"
+
+//console.log(nome)
+//console.log(idade)
+//console.log(país)
+
+//Retribuindo valores
+nome = "Gustavo"
+idade = 18
+//país = "Japão"
+console.log(nome)
+console.log(idade)
+//console.log(país)
+
+//================================
+//2. TIPO DE VARIÁVEIS: NUMBER (NÚMERO)
+//================================
+let numeroIntero = 10
+const numeroDecimal = 3.14
+
+console.log(numeroDecimal)
+console.log(numeroIntero)
+
+//================================
+//3. TIPO DE VARIÁVEIS: STRING (TEXTO)
+//================================
+let nome2 = "Romário"
+let mensagem = "Olá, eu sou o Romario aleatório!"
+
+console.log(nome2)
+console.log(mensagem)
+
+//=====================================
+//4. TIPO DE VARIÁVEIS; BOOLEAN (VERDADEIRO OU FALSO)
+//=====================================
+let estaChovendo = true
+let alunoReprovado = false
+
+console.log(estaChovendo)
+console.log(alunoReprovado)
---
+++ Variavel/desafio04.js
@@ -0,0 +1,9 @@
+let nome = "Onze"
+let idade = 18
+let porjeto = 0.11
+let mundo = "mundo invertido"
+let mortos = 68
+let comida = 5
+let Feliz = true
+
+console.log("Olá meu nome é " + nome + " e eu tenho " + idade + " anos sou um experimento cientifico o projeto " + porjeto + " e enfrento monstros que vivem em outro mundo o " + mundo + " Até agora matei " + mortos + " Demogorgon, logo em segui precisava de energia entao comi " + comida + " waffles isso me deixo " + Feliz + " de alegria!")
